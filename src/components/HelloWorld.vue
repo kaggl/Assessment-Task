7769f7c25bc3d18d0231e09a6bd4cca194042b8e
Robin <robinkaggl@hotmail.de>
2021-06-08 15:27:25 +0200

    added search icon

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,9 +1,13 @@
 <template>
   <div>
     <div class="search input-group mb-3">
-      <input type="text" @keyup.enter="send" v-model="input" class="form-control" placeholder="" aria-label="" aria-describedby="basic-addon1">
+      <input type="text" @keyup.enter="send" v-model="input" class="form-control" placeholder="Please Enter your Search terms here" aria-label="" aria-describedby="basic-addon1">
       <div class="input-group-prepend">
-        <button class="btn btn-outline-primary" @click="send"  type="button">Submit</button>
+        <button class="btn btn-outline-primary" @click="send"  type="button">
+          <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-search" viewBox="0 0 16 16">
+            <path d="M11.742 10.344a6.5 6.5 0 1 0-1.397 1.398h-.001c.03.04.062.078.098.115l3.85 3.85a1 1 0 0 0 1.415-1.414l-3.85-3.85a1.007 1.007 0 0 0-.115-.1zM12 6.5a5.5 5.5 0 1 1-11 0 5.5 5.5 0 0 1 11 0z"/>
+          </svg>
+        </button>
       </div>
     </div>
     <p>
