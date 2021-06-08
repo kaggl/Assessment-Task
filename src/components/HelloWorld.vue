a083cfff11fcbc408549479be5de6fd6fa2b3a14
Robin <robinkaggl@hotmail.de>
2021-06-08 12:43:17 +0200

    added table

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,6 +3,24 @@
     <h1>Assessment Task</h1>
     <input type="text" @keyup.enter="send" v-model="input" />
     <input type="button" @click="send" value="Submit" />
+    <table class="table">
+      <thead>
+        <tr>
+          <th scope="col">Label</th>
+          <th scope="col">Title</th>
+          <th scope="col">Quote</th>
+          <th scope="col">commentary</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-for="res in results" :key="res.legacy_pk">
+          <td>{{ res.display_label }}</td>
+          <td>{{ res.text.title }}</td>
+          <td>{{ res.zitat }}</td>
+          <td>{{ res.text.kommentar }}</td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -14,17 +32,22 @@ export default {
   data() {
     return {
       input: "",
+      results: [],
     };
   },
   methods: {
-
     async send() {
       console.log(this.input);
-      axios(`https://mmp.acdh-dev.oeaw.ac.at/api/stelle/?format=json&zitat_lookup=icontains&zitat=${this.input}`, {
-        // mode: 'cors',
+      axios("https://mmp.acdh-dev.oeaw.ac.at/api/stelle/", {
+        params: {
+          format: "json",
+          zitat_lookup: "icontains",
+          zitat: this.input,
+        }
       })
       .then((res) => {
-        console.log(res);
+        console.log(res.data);
+        this.results = res.data.results;
       })
       .catch((error) => {
         console.log(error);
@@ -33,7 +56,5 @@ export default {
   },
 };
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
 </style>
