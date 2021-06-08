110ee6bd4079b80d7246ca1a6662530fa729de56
Robin <robinkaggl@hotmail.de>
2021-06-08 13:09:01 +0200

    added loading indicator

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,8 +1,15 @@
 <template>
   <div>
     <h1>Assessment Task</h1>
-    <input type="text" @keyup.enter="send" v-model="input" />
-    <input type="button" @click="send" value="Submit" />
+    <p>
+      <input type="text" @keyup.enter="send" v-model="input" />
+      <input type="button" class="btn btn-outline-primary" @click="send" value="Submit" />
+    </p>
+    <p>
+      <span v-if="init">Please insert search query</span>
+      <span v-else-if="loading">loading...</span>
+      <span v-else>{{ count }} results found, {{ results.length }} shown</span>
+    </p>
     <table class="table">
       <thead>
         <tr>
@@ -33,11 +40,17 @@ export default {
     return {
       input: "",
       results: [],
+      loading: false,
+      count: 0,
+      init: true,
     };
   },
   methods: {
     async send() {
       console.log(this.input);
+      this.init = false;
+      this.loading = true;
+      console.log(this.loading);
       axios("https://mmp.acdh-dev.oeaw.ac.at/api/stelle/", {
         params: {
           format: "json",
@@ -48,13 +61,22 @@ export default {
       .then((res) => {
         console.log(res.data);
         this.results = res.data.results;
+        this.count = res.data.count;
+        this.loading = false;
       })
       .catch((error) => {
         console.log(error);
+        this.loading = false;
       });
     },
   },
 };
 </script>
 <style scoped>
+  th {
+    width: 35vw;
+  }
+  th:nth-of-type(2), th:first-of-type {
+    width: 15vw
+  }
 </style>
