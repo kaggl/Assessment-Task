47ab53a38f794e0a126e5a74a02a8ea9ed597ef6
Robin <robinkaggl@hotmail.de>
2021-06-16 12:12:40 +0200

    added working NavigationBar Links

--- src/components/NavigationBar.vue
+++ src/components/NavigationBar.vue
@@ -2,8 +2,8 @@
   <nav class="navbar navbar-expand-lg navbar-light">
     <div class="container">
       <!-- Your site title as branding in the menu -->
-      <a href="/" class="navbar-brand custom-logo-link" rel="home" itemprop="url"><img src="../assets/fundament_logo.svg" class="img-fluid" alt="Your Logo" itemprop="logo"></a>
-      <a class="navbar-brand site-title-with-logo" rel="home" href="#" title="Your Project" itemprop="url">Assessment Task</a>
+      <router-link to="/search/" class="navbar-brand custom-logo-link" rel="home"><img src="../assets/fundament_logo.svg" class="img-fluid" alt="Your Logo" itemprop="logo"></router-link>
+      <router-link to="/search/" class="navbar-brand site-title-with-logo" rel="home" title="Assessment Task">Assessment Task</router-link>
       <a href="https://github.com/kaggl/Assessment-Task">
         <button class="form-inline btn btn-outline-info">
           Repository
